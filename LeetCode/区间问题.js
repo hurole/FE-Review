a2747264d9573ad8e2e596429913dddb9e0dc06b
hurole <1192163814@qq.com>
2022-08-06 12:25:10 +0800

    fix: 区间合并

--- LeetCode/区间问题.js
+++ LeetCode/区间问题.js
@@ -1,46 +1,38 @@
 /**
- *  区间问题 
- *  区间是个对象包含两个属性 stat end 
+ *  区间问题leetcode 56
+ *
  */
 
-function handle(intervals) {
-  let result = [];
-  // 记录不能合并的区间
-  let record = [];
-  while (intervals.length) {
-    let item = intervals.shift()
-    for (let i = 0; i < intervals.length; i++) {
-      let curr = intervals[i];
-      if (isOverlap(item, curr)) {
-        item = {
-          start: item.start > curr.start ? curr.start : item.start,
-          end: item.end > curr.end ? item.end : curr.end
-        }
-      } else {
-        record.push(curr)
-      }
+function mergeRange(intervals) {
+  // 将区间按照起始值从小到大排序，获得有序区间数组
+  let sortIntervals = intervals.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  // 存放不能内合并区间
+  let merged = [];
+  merged.push(sortIntervals.shift());
+  // 循环遍历有序区间数组，如果可以合并就将合并后的区间替换merged
+  sortIntervals.forEach((item) => {
+    if (item[0] <= merged[merged.length - 1][1]) {
+      merged[merged.length - 1][1] =
+        item[1] - merged[merged.length - 1][1] > 0
+          ? item[1]
+          : merged[merged.length - 1][1];
+    } else {
+      merged.push(item);
     }
-    result.push(item)
-    intervals = record;
-    record = [];
-  }
-  return result;
-}
-// 检测first和second是否存在重叠
-function isOverlap(first, second) {
-  if ((first.start > second.end) || (second.start > first.end)) {
-    return false
-  }
-  return true;
+  });
+  return merged;
 }
 
 // test
 console.log("————————————————测试————————————————————————");
-let a = { start: 1, end: 9 }
-let b = { start: 2, end: 10 }
-let c = { start: 11, end: 12 }
-let d = { start: 12, end: 14 }
-let f = { start: 5, end: 9 }
+let a = [1, 2];
+let aa = [3, 5];
+let b = [2, 10];
+let c = [11, 12];
+let d = [12, 14];
+let f = [5, 9];
 
-let s = handle([a, b, c, d, f])
+let s = mergeRange([a, aa, b, c, d, f]);
 console.log(s);
